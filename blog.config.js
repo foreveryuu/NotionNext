aadbe89eb3ee3652be3968d61c3f1b809845d744
foreveryuu <119954620+foreveryuu@users.noreply.github.com>
2024-08-07 16:41:32 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -24,7 +24,7 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '这里是昵称', // 您的昵称 例如 tangly1024
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Forever', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '这是一个作者简介', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || '这里是网址', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '关键词1, 关键词2', // 网站关键词 英文逗号隔开
